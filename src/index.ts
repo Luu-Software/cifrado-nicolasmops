1ac40dcad8e7a375018804fb00a384d8f76c92f4
nicolasmops <mopsnicolas@gmail.com>
2026-06-02 15:49:30 -0300

    Consigna base ✅

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 import { preguntar } from './lib/consola.ts';
 import { cifrar, descifrar } from './lib/funcionesCriptográficas.ts';
 
-let palabra: string = ''; // Preguntar por la palabra a cifrar o descifrar
-let claveTexto: string = ''; // Preguntar por la clave de cifrado (número)
-let accion: string = ''; // Preguntar si se desea cifrar o descifrar (cifrar/descifrar)
+let palabra: string = preguntar('Ingrese su palabra: '); 
+let claveTexto: string = preguntar('Ingrese su clave:  '); 
+let accion: string = preguntar('Desea descifrar o cifrar: ');
 
-let resultado: string = ''; // COMPLETAR
-console.log(resultado);
+let resultado: string = '';
+
+if (accion === 'cifrar' || accion === 'Cifrar' ){   
+    resultado = cifrar(palabra, Number(claveTexto));
+    console.log('La palabra es: ' + resultado);
+
+    }
+else if(accion === 'descifrar' || accion === 'Descifrar'){
+    resultado = descifrar(palabra, Number(claveTexto));
+    console.log('La palabra es: ' + resultado);
+}
+else{
+    console.log('Ingresar una accion válida.')
+
+}
