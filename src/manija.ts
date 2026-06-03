544d6f732dcdc21e16e38f6c9f48779f039248c5
nicolasmops <mopsnicolas@gmail.com>
2026-06-03 16:00:16 -0300

    lio

--- src/manija.ts
+++ src/manija.ts
@@ -1,12 +1,18 @@
 import { preguntar } from './lib/consola.ts';
 
+let tarjetaTexto: string = preguntar('Ingresá el número de tarjeta: ');
+let tamañoPrefijo: string = preguntar('Ingrese el tamaño de su prefijo: ');
+let red: string = "";
+
 function obtenerPrefijo(numero: number, tamañoPrefijo: number): number {
-  return Number(tarjetaTexto) / 10 ^ 16 - tamañoPrefijo
+  return numero / 10 ** (16 - tamañoPrefijo);
 }
 
-let tarjetaTexto: string = preguntar('Ingresá el número de tarjeta: ');
+if (Number(tamañoPrefijo) === 2) {
+  if (obtenerPrefijo(Number(tarjetaTexto), Number(tamañoPrefijo)) === 34 
+   || obtenerPrefijo(Number(tarjetaTexto), Number(tamañoPrefijo)) === 37) {
+    red = "American Express";
+  }
+}
 
-let red: string = ''; // COMPLETAR
 console.log(red);
- // 
- 
